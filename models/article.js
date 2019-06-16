0dcbe97c9b8b2aee685cc678525c3b5cc108f82b
gk <georgepulikkayath@gmail.com>
2019-06-15 22:06:20 -0400

    files added

---
+++ models/article.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+
+// Create a Schema class with mongoose
+var Schema = mongoose.Schema;
+
+// Make LibrarySchema a Schema
+var ArticleSchema = new Schema({
+  // name: a unique string
+  title: {
+    type: String,
+    unique: true
+  },
+  link: {
+      type: String
+  },
+  summary:{
+     type:String
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
+});
+
+// Save the  model using the LibrarySchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the  model
+module.exports = Article;
